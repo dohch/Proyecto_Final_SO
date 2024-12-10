c2ace61cc0ac2c8048acacc038bd16223d214836
DOCK <franwillaa@gmail.com>
2024-12-10 09:27:43 -0500

    Update Documentation

--- boot/start.asm
+++ boot/start.asm
@@ -10,18 +10,23 @@ global _start, _kmain
 ;end_stors : 
 extern kmain, start_ctors, end_ctors, start_dctors, end_dctors 
 ;Macros de valores
+;Multiboot: Number for computer to know BOOT-MULTIBOOT
+;Flags : Flags number - i don't know
+;Checksum : To know the data integrity == 0
 %define MULTIBOOT_HEADER_MAGIC 0x1BADB002
 %define MULTIBOOT_HEADER_FLAGS 0x00000003
 %define CHECKSUM - (MULTIBOOT_HEADER_MAGIC+MULTIBOOT_HEADER_FLAGS)
 
+;Start changer the system
 _start:
 	jmp start
+;Define 4 bytes length
 align 4
 
-multiboot_header:
-dd MULTIBOOT_HEADER_MAGIC
-dd MULTIBOOT_HEADER_FLAGS
-dd CHECKSUM
+	multiboot_header:
+	dd MULTIBOOT_HEADER_MAGIC
+	dd MULTIBOOT_HEADER_FLAGS
+	dd CHECKSUM
 
 start:
 	push ebx
